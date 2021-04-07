420690fe6faa921490d4b28634f43afdc6e1d684
cherfychow <fycc1@st-andrews.ac.uk>
2021-04-07 11:03:07 +0100

    CC finished separating map and diversity trends into different apps

--- shinyMap/app.R
+++ shinyMap/app.R
@@ -5,7 +5,6 @@
 # Author: Cher Chow
 # Updated: 29 Mar 2021
 
-require(utils) # for choose directory
 require(shiny)
 require(shinyjs)
 #require(RMariaDB)
@@ -13,6 +12,7 @@ require(shinyjs)
 require(tidyverse)
 require(plotly)
 require(leaflet)
+require(sf)
 require(leaflet.providers)
 
 
@@ -24,7 +24,8 @@ setwd(file.choose() %>% dirname())
 # BioTIME color functions
 source('scale_gg_biotime.R')
 BT_datasets <- read.csv('working_data.csv', header=T) # table for dataset metadata
-study.extents <- readRDS('large_extent_studies.rds')
+study.extents <- readRDS('large_extent_studies.rds') # load the hex cell study extents
+# transform the spatial data to fit the original BioTIME WGS datum
 study.extents <- st_as_sf(study.extents) %>% st_set_crs("+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=km +no_defs") %>% 
   st_transform(., 4326)
 # study_coords <- read.csv('study_coords.csv', header=T) # table of dataset coordinates (can be > 1)
@@ -47,27 +48,6 @@ BT_datasets$REALM <- as.factor(BT_datasets$REALM)
 BT_datasets$BIOME_MAP <- as.factor(BT_datasets$BIOME_MAP)
 BT_datasets$CLIMATE <- as.factor(BT_datasets$CLIMATE)
 
-# get diversity indices data for trends
-a_div <- read.csv('alpha_s1.csv', header=T)
-# S species richness; varS variance in species richness among samples;
-# N summed abundances for all species in pooled samples;
-# varN variance among samples in summed abundances for all species;
-# SsqrtN, species richness divided by the square root of total summed abundances;
-# PIE probability of interspecific encounter; DomMc McNaughton dominance; 
-# expShannon exponential of Shannon diversity (also known as the Hill Number 1D;
-# Chao, Chao1 asymptotic species richness estimator; Chao2 bias corrected Chao1.
-
-b_div <- read.csv('beta_s2.csv', header=T)
-# Jaccard_B is the Jaccard similarity between the year’s pooled samples and the pooled sample of the first year in the time series (the time series baseline);
-# Horn_B similar to Jaccard_B but using Morisita-Horn distance;
-# Chao_B, as before but using Chao distance;
-# Pearson_B as before but using Pearson correlation
-
-a_div <- left_join(a_div, BT_datasets %>% select(ID=STUDY_ID, TAXA, REALM, CLIMATE), by='ID')
-b_div <- left_join(b_div, BT_datasets %>% select(ID=STUDY_ID, TAXA, REALM, CLIMATE), by='ID')
-
-
-
 
 # User Interface (Frontend) ---------------------------------------------------------------
 ui <- fluidPage(
@@ -79,86 +59,55 @@ ui <- fluidPage(
   useShinyjs(),
   
   div(id='mapwrap',
-  fluidRow(id='count',
-      div(class='stat', h1(textOutput('studies')), h4('studies')),
-      div(class='stat', h1(textOutput('years')), h4('years')),
-      div(class='stat', h1(textOutput('contributors')), h4('contributors')),
-      div(class='stat', h1(textOutput('taxa')), h4('taxa')),
-      div(class='stat', h1(textOutput('biome')), h4('biomes'))
-  ),
+    fluidRow(id='count',
+        div(class='stat', h1(textOutput('studies')), h4('studies')),
+        div(class='stat', h1(textOutput('years')), h4('years')),
+        div(class='stat', h1(textOutput('contributors')), h4('contributors')),
+        div(class='stat', h1(textOutput('taxa')), h4('taxa')),
+        div(class='stat', h1(textOutput('biome')), h4('biomes'))
+    ),
   
-  # Dataset map
-  fluidRow(id='maprow',
-           leafletOutput("StudyMap", height='100%'), #output map
-           
-           absolutePanel(id='control', draggable=T, # control panel
-                         h2('Filter datasets'),
-                         tags$div(class="accordion",
-                                  tags$input(id='tog_realm', type='checkbox', class='accordion-toggle', name='toggle', checked=NA),
-                                  tags$label(`for`='tog_realm', 'Realm'),
-                                  tags$section(checkboxGroupInput('Realm', label=NULL,
-                                                                  choiceNames=levels(BT_datasets$REALM),
-                                                                  selected=levels(BT_datasets$REALM),
-                                                                  choiceValues=levels(BT_datasets$REALM)))),
-                         
-                         tags$div(class="accordion",
-                                  tags$input(id='tog_taxa', type='checkbox', class='accordion-toggle', name='toggle'),
-                                  tags$label(`for`='tog_taxa', 'Taxa'),
-                                  tags$section(checkboxGroupInput('Taxa', label=NULL,
-                                                                  choiceNames=levels(BT_datasets$TAXA),
-                                                                  selected=levels(BT_datasets$TAXA),
-                                                                  choiceValues=levels(BT_datasets$TAXA)))),
-                         
-                         tags$div(class="accordion",
-                                  tags$input(id='tog_climate', type='checkbox', class='accordion-toggle', name='toggle'),
-                                  tags$label(`for`='tog_climate', 'Climate'),
-                                  tags$section(checkboxGroupInput('Climate', label=NULL,
-                                                                  choiceNames=levels(BT_datasets$CLIMATE),
-                                                                  selected=levels(BT_datasets$CLIMATE),
-                                                                  choiceValues=levels(BT_datasets$CLIMATE)))),
-                         
-                         tags$div(class="accordion",
-                                  tags$input(id='tog_dur', type='checkbox', class='accordion-toggle', name='toggle', checked=NA),
-                                  tags$label(`for`='tog_dur', 'Duration (yrs)'),
-                                  tags$section(sliderInput('Duration', label=NULL,
-                                                           min=2, max=130, value=c(2, 130),
-                                                           round = T, ticks = F, animate = FALSE))),
-                         actionButton('reset', 'Reset')
-           )
-  )),
-  
-  div(id='trendwrap',
-  # Diversity trends
-  fluidRow(id="trendrow",
-           column(width=3, 
-                  h2('Settings'),
-                  tags$div(class="accordion",
-                           tags$input(id='tog_metrics', type='checkbox', class='accordion-toggle', name='toggle', checked=NA),
-                           tags$label(`for`='tog_metrics', 'Choose metrics'),
-                           tags$section(selectInput('a_metric', 'Alpha metric', 
-                                                    c("Species richness" = 'S',
-                                                      "Total abundance" = "N",
-                                                      'Hill number' = 'expShannon')),
-                                        selectInput('b_metric', 'Beta metric', 
-                                                    c("Jaccard dissimilarity" = 'Jaccard_B',
-                                                      "Pearson correlation" = "Pearson_B")),
-                                        radioButtons('color_by', 'Color trends by', 
-                                                    c("Realm" = 'REALM',
-                                                      "Taxa" = "TAXA",
-                                                      "Climate" = 'CLIMATE'))
-                           )
-                  )
-           ),
-           
-           column(width=9,
-                  h1('Diversity trends'),
-                  h3('Alpha diversity'),
-                  plotlyOutput('alpha_div', width="90%", height='50vh'),
-                  h3('Beta diversity'),
-                  plotlyOutput('beta_div', width='90%', height='50vh')
-           )
-  ))
-)
+    # Dataset map
+    fluidRow(id='maprow',
+             leafletOutput("StudyMap", height='100%'), #output map
+             
+             absolutePanel(id='control', draggable=T, # control panel
+                           h2('Filter datasets'),
+                           tags$div(class="accordion",
+                                    tags$input(id='tog_realm', type='checkbox', class='accordion-toggle', name='toggle', checked=NA),
+                                    tags$label(`for`='tog_realm', 'Realm'),
+                                    tags$section(checkboxGroupInput('Realm', label=NULL,
+                                                                    choiceNames=levels(BT_datasets$REALM),
+                                                                    selected=levels(BT_datasets$REALM),
+                                                                    choiceValues=levels(BT_datasets$REALM)))),
+                           
+                           tags$div(class="accordion",
+                                    tags$input(id='tog_taxa', type='checkbox', class='accordion-toggle', name='toggle'),
+                                    tags$label(`for`='tog_taxa', 'Taxa'),
+                                    tags$section(checkboxGroupInput('Taxa', label=NULL,
+                                                                    choiceNames=levels(BT_datasets$TAXA),
+                                                                    selected=levels(BT_datasets$TAXA),
+                                                                    choiceValues=levels(BT_datasets$TAXA)))),
+                           
+                           tags$div(class="accordion",
+                                    tags$input(id='tog_climate', type='checkbox', class='accordion-toggle', name='toggle'),
+                                    tags$label(`for`='tog_climate', 'Climate'),
+                                    tags$section(checkboxGroupInput('Climate', label=NULL,
+                                                                    choiceNames=levels(BT_datasets$CLIMATE),
+                                                                    selected=levels(BT_datasets$CLIMATE),
+                                                                    choiceValues=levels(BT_datasets$CLIMATE)))),
+                           
+                           tags$div(class="accordion",
+                                    tags$input(id='tog_dur', type='checkbox', class='accordion-toggle', name='toggle', checked=NA),
+                                    tags$label(`for`='tog_dur', 'Duration (yrs)'),
+                                    tags$section(sliderInput('Duration', label=NULL,
+                                                             min=2, max=130, value=c(2, 130),
+                                                             round = T, ticks = F, animate = FALSE))),
+                           actionButton('reset', 'Reset')
+             ) # end control panel
+    ) # end row
+  ) # end wrapper
+) # end UI
 
 
 # Server -----------------------------------------------------------
@@ -206,7 +155,7 @@ server <- function(input, output) {
       setView(lng=0, lat=0, zoom=1.25) %>% 
       addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
       addPolygons(data=large.studies(), 
-                  fillOpacity=0.5, fillColor=~pal(DURATION), weight=0.5, color='#155f4933',
+                  fillOpacity=0.5, fillColor=~pal(DURATION), weight=1.2, color='#155f4933',
                   highlightOptions = highlightOptions(fillOpacity=0.9,fillColor='#cf7941', bringToFront = F),
                   popup = ~paste0("<h5>", TITLE,"</h5>",
                                   '<h6>', CONTACT_1, ', ', CONTACT_2,'</h6>',
@@ -237,42 +186,7 @@ server <- function(input, output) {
       addLegend(data=bind_rows(studies(), large.studies() %>% as_tibble()), position='bottomright', title='Duration', pal=pal, opacity=1,
                 values=~DURATION)
   })
-  
-
-  # Diversity trends --------------------------------------------------------
-  
-  # make working dataframes based on the filter options from the input
-  # alpha diversity data
-  a_div_data <- reactive({ 
-    a_div %>% filter(REALM %in% input$Realm & TAXA %in% input$Taxa & CLIMATE %in% input$Climate)
-  })
-  
-  # beta diversity data
-  b_div_data <- reactive({
-    b_div %>% filter(REALM %in% input$Realm & TAXA %in% input$Taxa & CLIMATE %in% input$Climate)
-  })
-  
-  output$alpha_div <- renderPlotly({
-    # alpha diversity gg object
-    alpha_div <- ggplot(a_div_data(), aes_string(x='Year', y=input$a_metric, group='ID')) +
-      geom_line(aes_string(color=input$color_by), alpha=0.7) +
-      labs(x='Year', y=names(input$a_metric)) +
-      scale_color_biotime(palette='realms', discrete=T, name=names(input$color_by)) +
-      theme_bw(base_size=14, base_family = 'Public Sans') + 
-      theme(panel.grid=element_blank(), axis.ticks=element_line(size=0.3))
-    ggplotly(alpha_div)
-  })
-  
-  output$beta_div <- renderPlotly({
-    beta_div <- ggplot(b_div_data(), aes_string(x='Year', y=input$b_metric, group='ID')) +
-      geom_line(aes_string(color=input$color_by), alpha=0.7) +
-      labs(x='Year', y=names(input$b_metric)) +
-      scale_color_biotime(palette='realms', discrete=T, name=names(input$color_by)) +
-      theme_bw(base_size=14, base_family = 'Public Sans') + theme(panel.grid=element_blank(), axis.ticks=element_line(size=0.3))
-    ggplotly(beta_div)
-  })
-  
-}
+}  
 
 # Run the application 
 shinyApp(ui = ui, server = server)
